d3e628628f5886e42072d4f1f77a117478f3712e
Arthur Vaz <arthurvaz@Arthurs-MacBook-Pro.local>
2024-01-12 21:29:00 -0300

    dados da funcao de densidade

---
+++ env_dist.R
@@ -0,0 +1,9 @@
+
+setwd("/Users/arthurvaz/Desktop/CEFETRJ - Mestrado/Anomalia/Anomalia/kmeans/janelas")
+gecco_result <- readRDS("gecco_windons.RDS")
+setwd("/Users/arthurvaz/Desktop/CEFETRJ - Mestrado/Anomalia/Anomalia/Dataset")
+load("gecco_sample.RData")
+
+source('/Users/arthurvaz/Desktop/CEFETRJ - Mestrado/Anomalia/Anomalia/probab_anomalias.R')
+
+probab(gecco_result,0.2,gecco_sample)
